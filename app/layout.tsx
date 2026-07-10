a879f87b406a65e2e26e8bc1b13559b0eaa749a4
VermaBytes <vpratik125@gmail.com>
2026-07-10 14:23:00 +0530

    add google adsense account verification meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -74,6 +74,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <meta name="google-adsense-account" content="ca-pub-7598958516745914" />
         <Script id="theme-init" strategy="beforeInteractive">
           {`
             (function() {
